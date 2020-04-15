f8e66a330af12c91e92094b96357a1f2549a0f4b
kugyu10 <kugyu10@gmail.com>
2020-04-15 17:19:32 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :admin_user, only: :destroy
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
 
@@ -41,6 +42,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "ユーザーを削除しました"
+    redirect_to users_url
+  end
+
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
@@ -56,5 +64,8 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
-
+    
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: "kugyu10",
+  email: "kugyu10@gmail.com",
+  password: "qwertyui9",
+  password_confirmation: "qwertyui9",
+  admin: true
+)
+
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "qwertyui9"
+
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password
+  )
+end
